ca4a909a8075be5c1e48e8196c7aa7544df6782f
Joshua Fernandes <fernandes.josh@gmail.com>
2020-12-30 14:37:00 -0500

    ver 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import React, { Component } from 'react'
 
 import './App.css';
-import * as _ from 'lodash'
+import { sample, indexOf } from 'lodash'
 
 export default class App extends Component {
   state = {
     gameTable: [],
-    cards: ['red1', 'red2', 'red3', 'red4', 'red5', 'red6', 'red7', 'red8', 'blue1', 'blue2', 'blue3', 'blue4', 'blue5', 'blue6', 'blue7', 'blue8', 'black1', 'white1', 'white2', 'white3', 'white4', 'white5', 'white6', 'white7', 'wildcard1'],
   }
 
   _generateTables() {
-    var usingCards = this.state.cards
+    var cards = ['red1', 'red2', 'red3', 'red4', 'red5', 'red6', 'red7', 'red8', 'blue1', 'blue2', 'blue3', 'blue4', 'blue5', 'blue6', 'blue7', 'blue8', 'black1', 'white1', 'white2', 'white3', 'white4', 'white5', 'white6', 'white7', 'wildcard1']
     var gameTable = []
 
     while(gameTable.length <= 25) {
-      var x = _.sample(usingCards)
-      usingCards.splice(_.indexOf(usingCards, x), 1)
+      var x = sample(cards)
+      cards.splice(indexOf(cards, x), 1)
       gameTable.push(x)
     }
 
@@ -32,55 +31,63 @@ export default class App extends Component {
     var fifthRow = [this.state.gameTable[20], this.state.gameTable[21], this.state.gameTable[22], this.state.gameTable[23], this.state.gameTable[24]]
 
     return (
-      <div className="App">
-          <button onClick={() => this._generateTables()}>Generate Table</button>
-          <table className="table">
-            <tr>
-              {
-                firstRow.map((item, index) => {
-                  return (
-                    <td className={`cell ${item}`}></td>
-                  )
-                })
-              }
-            </tr>
-            <tr>
-              {
-                secondRow.map((item, index) => {
-                  return (
-                    <td className={`cell ${item}`}></td>
-                  )
-                })
-              }
-            </tr>
-            <tr>
-              {
-                thirdRow.map((item, index) => {
-                  return (
-                    <td className={`cell ${item}`}></td>
-                  )
-                })
-              }
-            </tr>
-            <tr>
-              {
-                fourthRow.map((item, index) => {
-                  return (
-                    <td className={`cell ${item}`}></td>
-                  )
-                })
-              }
-            </tr>
-            <tr>
-              {
-                fifthRow.map((item, index) => {
-                  return (
-                    <td className={`cell ${item}`}></td>
-                  )
-                })
-              }
-            </tr>
-          </table>
+      <div className="container">
+        <h1>Codenames Generator</h1>
+          {
+            this.state.gameTable.length === 0 ?
+            <div />
+            :
+            <table className="table">
+              <tr>
+                {
+                  firstRow.map((item, index) => {
+                    return (
+                      <td className={`cell ${item}`}></td>
+                    )
+                  })
+                }
+              </tr>
+              <tr>
+                {
+                  secondRow.map((item, index) => {
+                    return (
+                      <td className={`cell ${item}`}></td>
+                    )
+                  })
+                }
+              </tr>
+              <tr>
+                {
+                  thirdRow.map((item, index) => {
+                    return (
+                      <td className={`cell ${item}`}></td>
+                    )
+                  })
+                }
+              </tr>
+              <tr>
+                {
+                  fourthRow.map((item, index) => {
+                    return (
+                      <td className={`cell ${item}`}></td>
+                    )
+                  })
+                }
+              </tr>
+              <tr>
+                {
+                  fifthRow.map((item, index) => {
+                    return (
+                      <td className={`cell ${item}`}></td>
+                    )
+                  })
+                }
+              </tr>
+            </table>
+          }
+          <div className="generate-button">
+            <button className="btn btn-lg btn-light" onClick={() => this._generateTables()}>Generate Table</button>
+          </div>
       </div>
     );
   }
